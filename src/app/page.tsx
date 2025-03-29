8ebfe92051dfbd2e5a6a97ce48e12df6f922ac8b
migarakavishan <migarakavishan43@gmail.com>
2025-03-29 20:09:52 +0530

    feat: implement user synchronization with Clerk and Prisma; update NavBar and add Prisma client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-
-
-
-export default function Home() {
+export default async function Home() {
   return (
     <div className="m-4">
       <h1 className="text-4xl font-bold">Home</h1>
-      
     </div>
   );
 }
